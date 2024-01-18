17f8967c79e1e84fe1336d2d80b86a28e02cb045
mahdihedari <mahdihedary2@gmail.com>
2024-01-19 01:47:21 +0330

    Add project files.

---
+++ FactoryMethod/Abilityes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern.FactoryMethod
+{
+    public abstract class Abilityes { 
+    }
+
+    public  class Imge :Abilityes { }
+    public  class AddFollower : Abilityes { }
+    public  class RemoveAccount : Abilityes { }
+    public  class UsePhoneNumber : Abilityes { }
+    public  class SendSms : Abilityes { }
+    public  class ShowViewer : Abilityes { }
+    public  class AddInstagramLink : Abilityes { }
+    public abstract class Resume { 
+    
+        List<Abilityes> _Abilityes = new List<Abilityes>();
+        public Resume() {
+            this.CreateAbilityes();
+
+        }
+        public List<Abilityes> Abilityes
+        {
+            get { return _Abilityes; }
+        }
+        public abstract void CreateAbilityes();
+
+    }
+    public class DemoAccount : Resume
+    {
+        public override void CreateAbilityes()
+        {
+            Abilityes.Add(new Imge());
+            Abilityes.Add(new RemoveAccount());
+            Abilityes.Add(new AddInstagramLink());
+         
+        }
+    }
+
+    public class RealAccount : Resume
+    {
+        public override void CreateAbilityes()
+        {
+            Abilityes.Add(new Imge());
+            Abilityes.Add(new RemoveAccount());
+            Abilityes.Add(new SendSms());
+            Abilityes.Add(new ShowViewer());
+           
+        }
+    }
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using pattern.FactoryMethod;
+
+List<Resume> products = new List<Resume>(2);
+products.Add(new DemoAccount());
+products.Add(new RealAccount());
+
+
+foreach (var product in products) { 
+
+   Console.WriteLine(product.GetType().Name);
+    foreach (Abilityes item in product.Abilityes)
+    {
+        Console.Write(item.GetType().Name + "  ");
+
+    }
+
+}
